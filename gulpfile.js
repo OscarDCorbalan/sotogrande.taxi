4be0fb87ee112206bff582db3ae216f72ef7dc56
Oscar D. Corbalan <oscardc@gmx.com>
2016-10-07 21:04:09 +0200

    feat: Setup LESS in Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,9 @@
+var gulp = require('gulp');  
 var less = require('gulp-less');
 var path = require('path');
 
-gulp.task('less', function () {
-  return gulp.src('./less/**/*.less')
+gulp.task('default', function () {
+  return gulp.src('./src/less/*.less')
     .pipe(less({
       paths: [ path.join(__dirname, 'less', 'includes') ]
     }))
