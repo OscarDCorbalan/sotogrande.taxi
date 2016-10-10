4778e3a7597a46048fc1499f1e98315d6a5d02f6
Oscar D. Corbalan <oscardc@gmx.com>
2016-10-10 20:54:23 +0200

    feat: Add Jade to gulp
    Closes #1

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@
 const path = require('path');
 const gulp = require('gulp');
 const less = require('gulp-less');
+const jade = require('gulp-jade');
 const cssmin = require('gulp-cssmin');
 const rename = require('gulp-rename');
 
@@ -22,19 +23,25 @@ gulp.task('watch', () =>
 	gulp.watch(SRC_FILES_LESS, ['less', 'cssmin'])
 );
 
-// if includes -> paths: [ path.join(__dirname, 'less', 'includes') ]
-
-gulp.task('less', () => {
+gulp.task('less', () =>
 	gulp.src(SRC_FILES_LESS)
 		.pipe(less().on('error', err => console.log(err)))
 		.pipe(gulp.dest(PUB_DIR.css))
-});
+);
 
-gulp.task('cssmin', function () {
+gulp.task('cssmin', () =>
     gulp.src(path.join(PUB_DIR.css, 'style.css'))
         .pipe(cssmin())
         .pipe(rename({suffix: '.min'}))
-        .pipe(gulp.dest(PUB_DIR.css));
-});
+        .pipe(gulp.dest(PUB_DIR.css))
+);
+
+gulp.task('jade', () =>
+	gulp.src(path.join(SRC_DIR.root, '*.jade'))
+		.pipe(jade({
+			client: true
+		}))
+		.pipe(gulp.dest(PUB_DIR.root))
+);
 
 gulp.task('default', ['less', 'cssmin', 'watch']);
